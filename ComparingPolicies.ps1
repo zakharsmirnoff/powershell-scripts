657927ab4b6d5e374833327c899cb67db7947270
zakharsmirnoff <89240654+zakharsmirnoff@users.noreply.github.com>
2021-08-27 16:55:22 +0800

    Add files via upload

---
+++ ComparingPolicies.ps1
@@ -0,0 +1,36 @@
+﻿$ReferenceObject = Get-CsTeamsMeetingPolicy -Identity Tag:AllOn #or you can import from CSV and change the required policy
+$DifferenceObject = Get-CsTeamsMeetingPolicy -Identity Global
+
+function SendTo-Teams {$JSONBody = [PSCustomObject][Ordered]@{
+"@type" = "MessageCard"
+"@context" = "http://schema.org/extensions"
+"summary" = "Here is the list of all the properties that were changed"
+"themeColor" = '0078D7'
+"title" = "Properties"
+"text" = $ChangedPrtopertiesToString
+}
+
+$TeamsMessageBody = ConvertTo-Json $JSONBody -Depth 30
+
+$parameters = @{
+"URI" = 'https://m365x770187.webhook.office.com/webhookb2/07188cea-abd7-4023-a0ec-c35265f664e8@a30446d5-8b09-4fcc-92e8-8a73ae50e8e7/IncomingWebhook/1d9ad3f1f28f423183ce5cf29016d511/58ab20bb-24e1-4902-afec-e02eac84dbd9'
+"Method" = 'POST'
+"Body" = $TeamsMessageBody
+"ContentType" = 'application/json'
+}
+Invoke-RestMethod @parameters}
+
+#getting the properties
+
+$properties = @(Get-CsTeamsMeetingPolicy -Identity Global | Get-Member -MemberType Property | Select-Object -ExpandProperty Name)
+$ChangedProperties = @()
+
+foreach ($property in $properties) {
+Compare-Object $ReferenceObject $DifferenceObject -Property "$property" | Where-Object {$_.SideIndicator -eq "=>"} | ForEach-Object {
+$ChangedProperties += $property} 
+}
+
+$ChangedPrtopertiesToString = $ChangedProperties -join ","
+
+if ($newarray.Length -gt 0) {
+SendToTeams} else {Write-Host "No changes"}
---
+++ GettingAuditLogsMCAS.ps1
@@ -0,0 +1,15 @@
+﻿#via the module for Cloud App Security we can get the list of all the acitivies for the applications
+
+$activities = @(Get-MCASActivity -AppName Microsoft_Skype_For_Business -ResultSetSize 50)
+
+foreach ($x in $activities) {
+if ($x.description -eq "Set-CsTeamsMeetingPolicy" -or "Set-CsTeamsMessagingPolicy") {
+Write-Host $x.rawDataJson}}
+
+#you can specify the activities for Teams admin center:
+
+$activities = @(Get-MCASActivity -AppName Microsoft_Teams -ResultSetSize 50)
+
+foreach ($x in $activities) {
+if ($x.description -eq "TeamsAdminChange") {
+Write-Host $x.rawDataJson}}
